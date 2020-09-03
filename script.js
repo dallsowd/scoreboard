04ab3574e6d1c353208203abbee917f1000cb238
dallsowd <dallsowd@gmail.com>
2020-09-03 12:08:05 +0930

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var loggedIn = false;
+
+function authenticate() {
+  var password = document.getElementById('password').value;
+  
+  loggedIn = login(password);
+  status();
+}
+
+function login(password) {
+    var storedPassword = '123';
+
+    return password == storedPassword;
+}
+
+function status() {
+  if(loggedIn) {
+    document.location.href = "fdffhjk6uhugywetehn5,mutytdrusefrdgbjr67gtgrtyfrvb6uk5f.html";
+  } else {
+    console.log('You are not in :(');
+  }
+}
