5c74baea563fab2e20a74999f03e3fc2d3aa3082
dallsowd <dallsowd@gmail.com>
2020-09-03 12:47:57 +0930

    user added

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 var loggedIn = false;
 
+document.addEventListener('keyup', (e) => {
+  if (e.code === "Enter") {
+    authenticate();
+  }
+});
+
 function authenticate() {
+  
+  var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
   
-  loggedIn = login(password);
-  status();
+  login(username, password);
 }
 
-function login(password) {
-    var storedPassword = '123';
+function login(username, password) {
+    var users = [{username: "dallsowd", password:"28118"},{username: "sowdedj", password:"28118"}];
 
-    return password == storedPassword;
+for (i = 0; i < users.length; i++) {
+  if(username == users[i].username && password == users[i].password) {
+    loggedIn = true;
+  }
+}
+    status();
 }
 
 function status() {
