8e6c97084ac59efce8bd89d47f18c154fa71fecb
dallsowd <dallsowd@gmail.com>
2020-09-04 09:38:11 +0930

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function authenticate() {
 }
 
 function login(username, password) {
-    var users = [{username: "guest", password:"password"},{username: "cazza", password:"beats"},{username: "damian", password:"securebattle"},{username: "paul", password:"securebattle"},{username: "verrall", password:"securebattle"},{username: "nicholas", password:"securebattle"},{username: "belton", password:"owner654"}];
+    var users = [{username: "guest", password:"password"},{username: "cazza", password:"beats"},{username: "damian", password:"securebattle"},{username: "paul", password:"securebattle"},{username: "verrall", password:"securebattle"},{username: "nicholas", password:"securebattle"},{username: "belton", password:"owner654"},{username:"campbell", password:"harley"}];
 
 for (i = 0; i < users.length; i++) {
   if(username == users[i].username && password == users[i].password) {
